131743efbbd599c472236f7793e18866c3066341
Gabriel David <devgabrieldavid@gmail.com>
2024-03-05 11:32:15 -0300

    criar models, conexao com bd, rotas e controllers

---
+++ PrimeiraAPI/Infra/ConnectionContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiraAPI.Model;
+
+namespace PrimeiraAPI.Infra
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(
+                "Server=localhost;" +
+                "Port=5432;Database=DataAPI;" +
+                "User Id=postgres;" +
+                "Password=123456;");
+    }
+}
